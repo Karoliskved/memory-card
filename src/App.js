647130aa5407f4bcb98ebb619bbbb9f38e54f4c0
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-09-05 14:00:53 +0300

    randomize card display order

--- src/App.js
+++ src/App.js
@@ -6,16 +6,39 @@ import './App.css';
 
 const App = (props) => {
   const [images, setImages] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+  const [imagesArray, setImagesArray] = useState(
+    shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+  );
   const [failStatus, setFailStatus] = useState(false);
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  function shuffle(array) {
+    // fisher-yates shuffle
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
   const handleClick = (e) => {
-    // console.log(images);
     if (!images.includes(parseInt(e.target.id))) {
       console.log('fail');
       setFailStatus(true);
       setImages([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
       setCurrentScore(0);
+      setImagesArray(shuffle(imagesArray));
     } else {
       const newArray = images.filter(
         (element) => element !== parseInt(e.target.id)
@@ -26,6 +49,7 @@ const App = (props) => {
       /* if (currentScore > bestScore) {
         setBestScore(currentScore);
       }*/
+      setImagesArray(shuffle(imagesArray));
     }
   };
   useEffect(() => {
@@ -34,7 +58,7 @@ const App = (props) => {
     }
   }, [bestScore, currentScore]);
   const test = () => {
-    console.log(images);
+    console.log(imagesArray);
   };
 
   return (
@@ -45,7 +69,7 @@ const App = (props) => {
         currentScore={currentScore}
         bestScore={bestScore}
       />
-      <Main handleClick={handleClick} />
+      <Main handleClick={handleClick} shuffledArray={imagesArray} />
       <button onClick={test}>test</button>
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,20 +2,21 @@ import Card from './Card';
 
 const Main = (props) => {
   const handleClick = props.handleClick;
+  const randomImage = props.shuffledArray;
   return (
     <div className="Main">
-      <Card image={1} handleClick={handleClick} />
-      <Card image={2} handleClick={handleClick} />
-      <Card image={3} handleClick={handleClick} />
-      <Card image={4} handleClick={handleClick} />
-      <Card image={5} handleClick={handleClick} />
-      <Card image={6} handleClick={handleClick} />
-      <Card image={7} handleClick={handleClick} />
-      <Card image={8} handleClick={handleClick} />
-      <Card image={9} handleClick={handleClick} />
-      <Card image={10} handleClick={handleClick} />
-      <Card image={11} handleClick={handleClick} />
-      <Card image={12} handleClick={handleClick} />
+      <Card image={randomImage[0]} handleClick={handleClick} />
+      <Card image={randomImage[1]} handleClick={handleClick} />
+      <Card image={randomImage[2]} handleClick={handleClick} />
+      <Card image={randomImage[3]} handleClick={handleClick} />
+      <Card image={randomImage[4]} handleClick={handleClick} />
+      <Card image={randomImage[5]} handleClick={handleClick} />
+      <Card image={randomImage[6]} handleClick={handleClick} />
+      <Card image={randomImage[7]} handleClick={handleClick} />
+      <Card image={randomImage[8]} handleClick={handleClick} />
+      <Card image={randomImage[9]} handleClick={handleClick} />
+      <Card image={randomImage[10]} handleClick={handleClick} />
+      <Card image={randomImage[11]} handleClick={handleClick} />
     </div>
   );
 };
