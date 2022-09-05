613cbb70ba310461255d6c9b6bdfc2d848e035ba
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-09-05 12:59:07 +0300

    add logic which keeps track of the score and round status

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Scoreboard from './components/Scoreboard';
 import Main from './components/Main';
 import './App.css';
 
 const App = (props) => {
+  const [images, setImages] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+  const [failStatus, setFailStatus] = useState(false);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const handleClick = (e) => {
+    // console.log(images);
+    if (!images.includes(parseInt(e.target.id))) {
+      console.log('fail');
+      setFailStatus(true);
+      setImages([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+      setCurrentScore(0);
+    } else {
+      const newArray = images.filter(
+        (element) => element !== parseInt(e.target.id)
+      );
+      setImages(newArray);
+      setFailStatus(false);
+      setCurrentScore(currentScore + 1);
+      /* if (currentScore > bestScore) {
+        setBestScore(currentScore);
+      }*/
+    }
+  };
+  useEffect(() => {
+    if (currentScore > bestScore) {
+      setBestScore(currentScore);
+    }
+  }, [bestScore, currentScore]);
+  const test = () => {
+    console.log(images);
+  };
+
   return (
     <div className="App">
       <Header />
-      <Scoreboard />
-      <Main />
+      <Scoreboard
+        isFail={failStatus}
+        currentScore={currentScore}
+        bestScore={bestScore}
+      />
+      <Main handleClick={handleClick} />
+      <button onClick={test}>test</button>
     </div>
   );
 };
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,16 +1,21 @@
 const Scoreboard = (props) => {
-  return (
-    <div className="Scoreboard">
-      <div className="counter">
-        <div>Best score:</div>
-        <div className="bestScoreCounter">0</div>
+  if (props.isFail) {
+    console.log('test');
+    return <div className="Scoreboard fail">Unlucky! try again.</div>;
+  } else {
+    return (
+      <div className="Scoreboard">
+        <div className="counter">
+          <div>Best score:</div>
+          <div className="bestScoreCounter">{props.bestScore}</div>
+        </div>
+        <div className="counter">
+          <div>Current scores:</div>
+          <div className="currentScoreCounter">{props.currentScore}</div>
+        </div>
       </div>
-      <div className="counter">
-        <div>Current scores:</div>
-        <div className="currentScoreCounter">0</div>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Scoreboard;
